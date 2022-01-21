77aae3e63fb73304d05cce6f13cef07b634c6b33
gemuz_home <gemuzkm@gmail.com>
2022-01-21 20:45:41 +0300

    add entity User class

--- src/main/java/by/tms/servlet/FirstServlet.java
+++ src/main/java/by/tms/servlet/FirstServlet.java
@@ -6,13 +6,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
-@WebServlet(urlPatterns = "/test")
+@WebServlet(urlPatterns = "/test", loadOnStartup = 0, name = "FirstServlet") //инициализация во время запуска, размер инта, чем меньше, тем выше приоритет
 public class FirstServlet extends HttpServlet {
+
+////    AtomicInteger count = 0;
+//
+//    @Override
+//    public void init() throws ServletException {
+//        System.out.println("Hello Servlet");
+//    }
+//
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.service(req, resp);
+////        count++;
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        super.destroy();
+//    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         String age = req.getParameter("age");
+
         resp.getWriter().println("Hello" + name + " " + age);
     }
 }
---
+++ src/main/java/listener/Listener.java
@@ -0,0 +1,49 @@
+package listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+// application scope / servlet context (живет все время сервлета)
+// session (время сессия, живет 30 минут, если нет активных запросов)
+// request scope (от запроса до ответа)
+// page scope
+
+
+@WebListener
+public class Listener implements ServletContextListener, HttpSessionListener, HttpSessionAttributeListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent event) {
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent event) {
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent event) {
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+    }
+
+    //дописать из проекта
+}
