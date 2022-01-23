0bdf98378b17eeee95cea2740e07d85c84c6e620
gemuz_home <gemuzkm@gmail.com>
2022-01-23 20:56:06 +0300

    class CalculatorFilter init

--- src/main/java/by/tms/servlet/CalculatorServlet.java
+++ src/main/java/by/tms/servlet/CalculatorServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/calculator")
+@WebServlet(urlPatterns = "/calculator", name = "CalculatorServlet")
 public class CalculatorServlet extends HttpServlet {
 
     @Override
