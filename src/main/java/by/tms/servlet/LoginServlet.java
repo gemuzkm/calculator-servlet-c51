25efc3adbf041dc5b819c0748d90fd01a5b130ef
gemuz_home <gemuzkm@gmail.com>
2022-01-23 22:21:13 +0300

    class LoginServlet add session

--- src/main/java/by/tms/servlet/LoginServlet.java
+++ src/main/java/by/tms/servlet/LoginServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/login", name = "LoginServlet")
@@ -13,5 +14,7 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
+        HttpSession session = req.getSession();
+        session.setAttribute("login", login);
     }
 }
