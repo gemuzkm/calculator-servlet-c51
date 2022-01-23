f753dc353d7f54ba144a1ec2f2f3eeee99f6bdc1
gemuz_home <gemuzkm@gmail.com>
2022-01-24 00:33:26 +0300

    class LogoutServlet fix with session

--- src/main/java/by/tms/servlet/LogoutServlet.java
+++ src/main/java/by/tms/servlet/LogoutServlet.java
@@ -14,7 +14,11 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        session.invalidate();
-        resp.getWriter().println("Logout successfully");
+        if (session.getAttribute("login") != null) {
+            session.invalidate();
+            resp.getWriter().println("Logout successfully");
+        } else {
+            resp.getWriter().println("You were not authorized");
+        }
     }
 }
