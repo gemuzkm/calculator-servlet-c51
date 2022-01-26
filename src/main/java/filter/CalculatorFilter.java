12a64b1e0e669f299f40d8b21c0cb9c1f24f4fe4
gemuz_work <gemuzkm@gmail.com>
2022-01-26 13:43:24 +0300

    class CalculatorFilter req by POST

--- src/main/java/filter/CalculatorFilter.java
+++ src/main/java/filter/CalculatorFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(servletNames = "CalculatorServlet")
@@ -16,27 +17,29 @@ public class CalculatorFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-//        if (req.getMethod().equals("POST")) {
-//
-//        }
-//        <p>${requestScope.message}</p> - SSR, page scope
 
+        //        <p>${requestScope.message}</p> - SSR, page scope
 
-        ValueValidation valueValidation = new ValueValidation();
-        OperationValidation operationValidation = new OperationValidation();
+        if (req.getMethod().equals("POST")) {
+            ValueValidation valueValidation = new ValueValidation();
+            OperationValidation operationValidation = new OperationValidation();
 
-        String valueFirst = req.getParameter("value1");
-        String valueSecond = req.getParameter("value2");
-        String operation = req.getParameter("operation");
+            String valueFirst = req.getParameter("value1");
+            String valueSecond = req.getParameter("value2");
+            String operation = req.getParameter("operation");
 
-        if (valueValidation.isNull(valueFirst) || valueValidation.isNull(valueSecond) || valueValidation.isNull(operation)) {
-            res.getWriter().println("Incorrect parameters");
-        } else if (!valueValidation.isNumber(valueFirst) || !valueValidation.isNumber(valueSecond)) {
-            res.getWriter().println("Transmitted parameters are not numbers");
-        } else if (!operationValidation.supportedOperation(operation)) {
-            res.getWriter().println("Operation unsupported");
-        } else {
-            chain.doFilter(req, res);
+            if (valueValidation.isNull(valueFirst) || valueValidation.isNull(valueSecond) || valueValidation.isNull(operation)) {
+                HttpSession session = req.getSession();
+                session.setAttribute("msgError", "Incorrect parameters");
+            } else if (!valueValidation.isNumber(valueFirst) || !valueValidation.isNumber(valueSecond)) {
+                HttpSession session = req.getSession();
+                session.setAttribute("msgError", "Transmitted parameters are not numbers");
+            } else if (!operationValidation.supportedOperation(operation)) {
+                HttpSession session = req.getSession();
+                session.setAttribute("msgError", "Operation unsupported");
+            } else {
+                chain.doFilter(req, res);
+            }
         }
     }
 }
