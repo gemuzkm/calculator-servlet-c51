5ff573a1a10baa1efdb6fabbe29499379b4409fc
gemuz_home <gemuzkm@gmail.com>
2022-01-28 00:03:07 +0300

    package entity add value

--- src/main/java/entity/ValueOne.java
+++ src/main/java/entity/ValueOne.java
@@ -1,4 +1,17 @@
 package entity;
 
 public class ValueOne {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public ValueOne(String value) {
+        this.value = value;
+    }
 }
--- src/main/java/entity/ValueTwo.java
+++ src/main/java/entity/ValueTwo.java
@@ -1,4 +1,17 @@
 package entity;
 
 public class ValueTwo {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public ValueTwo(String value) {
+        this.value = value;
+    }
 }
