b420fbcca372c2d5d4cd5ce7fc735f020e7e6ce4
gemuz_home <gemuzkm@gmail.com>
2022-01-30 14:58:47 +0300

    class DelUserFilter init

--- src/main/java/by/tms/servlet/DelUserServlet.java
+++ src/main/java/by/tms/servlet/DelUserServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/deluser")
+@WebServlet(urlPatterns = "/deluser", name = "DelUserServlet")
 public class DelUserServlet extends HttpServlet {
 
     @Override
