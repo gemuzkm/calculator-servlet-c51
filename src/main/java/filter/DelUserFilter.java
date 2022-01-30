25c41dc5b74c7f39ca4245a4f1b7502c6be6fd60
gemuz_home <gemuzkm@gmail.com>
2022-01-30 14:59:47 +0300

    class DelUserFilter init admin access only

--- src/main/java/filter/DelUserFilter.java
+++ src/main/java/filter/DelUserFilter.java
@@ -1,4 +1,36 @@
 package filter;
 
-public class DelUserFilter {
+import entity.User;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter (servletNames = "DelUserServlet")
+public class DelUserFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = req.getSession();
+        User user = new User();
+
+        if (session.getAttribute("user") != null) {
+            user = (User) session.getAttribute("user");
+        } else {
+            req.setAttribute("mgsError", "No access");
+            req.getServletContext().getRequestDispatcher("/pages/index.jsp").forward(req, res);
+        }
+
+        if (user.getRole() != 0) {
+            req.setAttribute("mgsError", "No access");
+            req.getServletContext().getRequestDispatcher("/pages/index.jsp").forward(req, res);
+        }
+
+        chain.doFilter(req, res);
+    }
 }
